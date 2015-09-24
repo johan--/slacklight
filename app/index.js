1e088e48041ea49eaa36e232fd1b036219457ac9
Shane Rogers <shane@repllabs.com>
2015-09-24 00:20:38 -0700

    add menu click handler on ig panels

--- app/index.js
+++ app/index.js
@@ -1,5 +1,42 @@
 import request from 'superagent';
+import swig from 'swig';
 
-(() => {
-  console.log('APP TRIGGERED');
+window.slacklight = (() => {
+  let init = () => {
+    addClickHandlersForClass("menu-icon", showContent)
+    addClickHandlersForClass("ig-image", showLightbox)
+  }
+
+  let addClickHandlersForClass = (className, fn) => {
+    let els = document.getElementsByClassName(className);
+    addHandler("click", els, fn)
+  }
+
+  let addHandler = (type, collection, fn) => {
+    for (var i = 0; i < collection.length; i++) {
+      collection[i].addEventListener(type, fn, false);
+    }
+  }
+
+  let showContent = (e) => {
+    let menu = e.currentTarget
+    let contentId = menu.getAttribute('data');
+    let content = document.getElementById(contentId);
+    content.classList.toggle("invisible");
+  }
+
+  let showLightbox = (e) => {
+    let lightbox = document.getElementById('lightbox');
+    lightbox.classList.toggle("invisible");
+    let img = e.currentTarget
+    let imageId = menu.getAttribute('data');
+    let photo = PhotoStore.get(imageId);
+
+    let lightboxContent = swig.renderFile('../views/lightbox.html', {photo: photo});
+
+
+    let content = document.getElementById(contentId);
+  }
+
+  window.addEventListener('load', init, false);
 }())
