235877931a907272663ddca9dd8d999986431138
Shane Rogers <shane@repllabs.com>
2015-09-24 21:20:43 -0700

    Add handlers and stylings for the modal

--- app/index.js
+++ app/index.js
@@ -2,11 +2,23 @@ import _ from 'lodash';
 import request from 'superagent';
 import swig from 'swig';
 import PhotoStore from '../lib/stores/photo-store';
+import Slacklight from './slacklight.js';
 
 window.slacklight = (() => {
   let init = () => {
-    addClickHandlersForClass("menu-icon", showContent)
-    addClickHandlersForClass("ig-image", showLightbox)
+    addClickHandlersForClass("menu-icon", showContent);
+    addClickHandlersForClass("ig-image", showLightbox);
+    addModalHideHandler();
+  }
+
+  let addModalHideHandler = () => {
+    let closeIcon = document.getElementById('close-icon');
+    closeIcon.addEventListener('click', toggleModal, false);
+  }
+
+  let toggleModal = () => {
+    let lightbox = document.getElementsByClassName('modal')[0];
+    lightbox.classList.toggle("visible");
   }
 
   let addClickHandlersForClass = (className, fn) => {
@@ -28,14 +40,42 @@ window.slacklight = (() => {
   }
 
   let showLightbox = (e) => {
-    let lightbox = document.getElementsByClassName('modal')[0];
-    lightbox.classList.toggle("visible");
+    toggleModal();
     let img = e.currentTarget
     let imageId = img.getAttribute('data');
     let photo = _.find(window.__allPhotos, {id: imageId})
 
+    // Set Title
     let title = document.getElementById('post-title');
     title.innerHTML = photo.caption;
+    // Set Image
+    let modalImage = document.getElementsByClassName('modal-image')[0];
+    modalImage.setAttribute('src', photo.images)
+
+    // Set toggle indexes
+    let photoIndex = _.findIndex(window.__allPhotos, {id: imageId})
+    let prevNav = document.getElementsByClassName('prev-nav-image')[0];
+    let nextNav = document.getElementsByClassName('next-nav-image')[0];
+
+    let prevIndex;
+    let nextIndex;
+    if (photoIndex === 0) {
+      prevIndex = prevNav.setAttribute('data', window.__allPhotos.length-1)
+    } else if (photoIndex === window.__allPhotos.length-1) {
+      nextIndex = 0;
+    }
+
+    if (prevIndex) {
+      prevNav.setAttribute('data', prevIndex)
+    } else {
+      prevNav.setAttribute('data', photoIndex-1)
+    }
+
+    if (nextIndex) {
+      nextNav.setAttribute('data', nextIndex)
+    } else {
+      nextNav.setAttribute('data', photoIndex+1)
+    }
   }
 
   window.addEventListener('load', init, false);
