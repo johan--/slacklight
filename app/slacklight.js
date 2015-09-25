7d24f2ed5ff77f11dab369ed8a50ac58b50a7889
Shane Rogers <shane@repllabs.com>
2015-09-25 00:30:20 -0700

    pass number instead of option string to indexPairs

--- app/slacklight.js
+++ app/slacklight.js
@@ -4,14 +4,14 @@ module.exports = {
     let prev, next;
 
     if (currentIndex === 0) {
-      prev = parseFloat(lastIndex)
-      next = parseFloat(currentIndex)+1
+      prev = lastIndex
+      next = currentIndex+1
     } else if (currentIndex === lastIndex ) {
-      prev = parseFloat(currentIndex)-1
+      prev = currentIndex-1
       next = 0
     } else {
-      next = parseFloat(currentIndex)+1
-      prev = parseFloat(currentIndex)-1
+      next = currentIndex+1
+      prev = currentIndex-1
     }
 
     return {
